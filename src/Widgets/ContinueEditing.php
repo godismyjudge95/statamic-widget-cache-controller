06c8faadbf0694fa2fbdf3f9bd6fd42ad011bc77
Alexander Stoffel <alex@stoffel.io>
2021-10-04 12:45:09 +0700

    com poser

--- src/Widgets/ContinueEditing.php
+++ src/Widgets/ContinueEditing.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Webographen\ContinueEditing\Widgets;
+namespace Webographen\CacheController\Widgets;
 
 use Statamic\Widgets\Widget;
 use Statamic\Facades\Entry;
 use Statamic\Facades\Collection as CollectionAPI;
 
-class ContinueEditing extends Widget
+class CacheController extends Widget
 {
     /**
      * The HTML that should be shown in the widget.
@@ -15,35 +15,6 @@ class ContinueEditing extends Widget
      */
     public function html()
     {
-        $collections = $this->config('collections', '*');
-        $limit =  $this->config('limit', 5);
-
-        if (strpos("*", $collections) !== false) {
-
-            $results = Entry::query()
-                            ->orderBy('updated_at', 'desc')
-                            ->limit($limit)
-                            ->get();
-
-        } else {
-
-            // make sure all collections exist
-            foreach (explode("|", $collections) as $collection) {
-                if (!CollectionAPI::handleExists($collection)) {
-                    return "Error: Collection [$collection] doesn't exist.";
-                }
-            }
-
-            $results = Entry::query()
-                            ->whereIn('collection', explode("|", $collections))
-                            ->orderBy('updated_at', 'desc')
-                            ->limit($limit)
-                            ->get();
-
-        }
-
-        return view('webographen::widgets.continue_editing', [
-            'results' => $results
-        ]);
+        return view('webographen::widgets.cache_controller');
     }
 }
